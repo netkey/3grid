f13efc38edf9a1d86e13f5b9f0ecef055d69469c
Zihua Ye <zihua.ye@qq.com>
2017-06-21 18:02:53 +0800

    201706211802

--- route/route.go
+++ route/route.go
@@ -1,12 +1,39 @@
-package grid
+package grid_route
 
-type Route_QA struct {
-	SrcIP   net.IP
-	DstDN   string
-	DstIP   net.IP
-	DstPath *Route_Path
+type Route_DB struct {
+	CacheServers map[int]Server_List_Record
+	NodeServers  map[int]Server_List_Record
+	Nodes        map[int]Node_List_Record
+	Domains      map[string]Domain_List_Record
+	Routes       map[string][string]Route_List_Record
 }
 
-type Route_Path struct {
-	Way map[int]int
+type Server_List_Record struct {
+	ServerIp    int
+	ServerGroup string
+	NodeId      int
+	Usage       int
+	Status      int
+	Weight      int
+}
+
+type Node_List_Record struct {
+	NodeId       int
+	NodeCapacity int
+	Usage        int
+	Status       int
+	Priority     int
+}
+
+type Domain_List_Record struct {
+	DomainId    int
+	DomainName  string
+	ServerGroup string
+}
+
+type Route_List_Record struct {
+	DomainName   string
+	ClientRegion string
+	NodeIds      string
+	Ext          string
 }
