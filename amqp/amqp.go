f883938c8faad651cb3af28b5979c71948fb6801
Zihua Ye <zihua.ye@qq.com>
2017-08-21 23:37:54 +0800

    201708212338

--- amqp/amqp.go
+++ amqp/amqp.go
@@ -174,12 +174,13 @@ func (c *AMQP_Broadcaster) Shutdown() error {
 }
 
 func (c *AMQP_Broadcaster) amqp_handle_b(deliveries <-chan amqp.Delivery, done chan error) {
+	var err error
 	var msg AMQP_Message
 	var cmds = &Cmds{}
 	var params = make([]reflect.Value, 2)
 
 	for d := range deliveries {
-		if err := Transmsg(d.Body, &msg); err != nil {
+		if err = Transmsg(d.Body, &msg); err != nil {
 			log.Printf("transmsg: %s", err)
 			continue
 		}
@@ -194,6 +195,15 @@ func (c *AMQP_Broadcaster) amqp_handle_b(deliveries <-chan amqp.Delivery, done c
 			params[0] = reflect.ValueOf(cmds)
 			params[1] = reflect.ValueOf(&msg)
 			fn.Func.Call(params)
+		}
+		//need ack?
+		if msg.Ack == true {
+			var _param = make(map[string]string)
+			var _msg1 []string
+
+			if err = Sendmsg("", AMQP_CM_ACK, &_param, msg.Object, &_msg1, "", msg.Sender, msg.ID); err != nil {
+				log.Printf("error sending ack msg: %s", err)
+			}
 		}
 	}
 	log.Printf("handle: deliveries channel closed")
@@ -376,7 +386,6 @@ func (c *AMQP_Director) amqp_handle_d(deliveries <-chan amqp.Delivery, done chan
 				log.Printf("error sending ack msg: %s", err)
 			}
 		}
-
 	}
 	log.Printf("handle: deliveries channel closed")
 	done <- nil
