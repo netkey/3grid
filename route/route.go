10d6b8d8088b2bd7cb0b67372b78137ce519f78c
Zihua Ye <zihua.ye@qq.com>
2017-09-20 17:52:42 +0800

    201709201752

--- route/route.go
+++ route/route.go
@@ -1,6 +1,9 @@
 package grid_route
 
+import G "3grid/tools/globals"
+import "log"
 import "net"
+import "sync"
 
 var Version string
 var Db_file string
@@ -11,6 +14,8 @@ type Route_db struct {
 	Nodes   map[uint]Node_List_Record             //uint for nodeid
 	Domains map[string]Domain_List_Record         //string for domain name
 	Routes  map[string]map[uint]Route_List_Record //string for AreaCode, uint for RoutePlan ID
+	Locks   map[string]*sync.RWMutex
+	Chan    chan map[string][]string
 }
 
 type Server_List_Record struct {
@@ -54,4 +59,69 @@ type PW_List_Record struct {
 }
 
 func (rt_db *Route_db) RT_db_init() {
+	rt_db.Servers = make(map[string]Server_List_Record)
+	rt_db.Nodes = make(map[uint]Node_List_Record)
+	rt_db.Domains = make(map[string]Domain_List_Record)
+	rt_db.Routes = make(map[string]map[uint]Route_List_Record)
+
+	rt_db.Locks = map[string]*sync.RWMutex{"servers": new(sync.RWMutex), "nodes": new(sync.RWMutex), "domains": new(sync.RWMutex), "routes": new(sync.RWMutex)}
+	rt_db.Chan = make(chan map[string][]string, 100)
+
+	if G.Debug {
+		log.Printf("Initializing route db..")
+	}
+
+	go rt_db.UpdateRoutedb()
+}
+
+func (rt_db *Route_db) UpdateRoutedb() error {
+	for {
+		m := <-rt_db.Chan
+		for t, a := range m {
+			rt_db.Locks[t].Lock()
+			switch t {
+			case "domains":
+				rt_db.Conver_Domain_Record(a)
+			case "servers":
+				rt_db.Conver_Server_Record(a)
+			case "nodes":
+				rt_db.Conver_Node_Record(a)
+			case "routes":
+				rt_db.Conver_Route_Record(a)
+			default:
+			}
+			rt_db.Locks[t].Unlock()
+		}
+	}
+
+	return nil
+}
+
+func (rt_db *Route_db) Conver_Domain_Record(a []string) {
+	var r Domain_List_Record
+	var k string
+
+	rt_db.Domains[k] = r
+}
+
+func (rt_db *Route_db) Conver_Server_Record(a []string) {
+	var r Server_List_Record
+	var k string
+
+	rt_db.Servers[k] = r
+}
+
+func (rt_db *Route_db) Conver_Node_Record(a []string) {
+	var r Node_List_Record
+	var k uint
+
+	rt_db.Nodes[k] = r
+}
+
+func (rt_db *Route_db) Conver_Route_Record(a []string) {
+	var r Route_List_Record
+	var k string
+	var nid uint
+
+	rt_db.Routes[k][nid] = r
 }
