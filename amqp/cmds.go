e39becea9fda78fc514ab514f5fff7481761c2f7
jerry <r_jerry@126.com>
2017-10-13 11:48:05 +0800

    Add, Delete

--- amqp/cmds.go
+++ amqp/cmds.go
@@ -18,9 +18,15 @@ func (c *Cmds) Ka(msg *AMQP_Message) error {
 }
 
 func (c *Cmds) Add(msg *AMQP_Message) (err error) {
-	log.Printf("Processing Add cmd..")
-	log.Printf("Adding: %+v", (*msg.Msg1))
-	return nil
+	defer func() {
+		if pan := recover(); pan != nil {
+			msg1 := fmt.Sprintf("%s", pan)
+			err = errors.New(msg1)
+		}
+	}()
+	db_type := msg.Object
+	*RT.Chan <- map[string]map[string]map[string]map[string][]string{db_type: *msg.Msg1}
+	return err
 }
 
 func (c *Cmds) Ver(msg *AMQP_Message) error {
@@ -32,12 +38,36 @@ func (c *Cmds) Ver(msg *AMQP_Message) error {
 func (c *Cmds) Update(msg *AMQP_Message) (err error) {
 	defer func() {
 		if pan := recover(); pan != nil {
-			msg := fmt.Sprintf("%s", pan)
-			err = errors.New(msg)
+			msg1 := fmt.Sprintf("%s", pan)
+			err = errors.New(msg1)
 		}
 	}()
 	db_type := msg.Object
 	*RT.Chan <- map[string]map[string]map[string]map[string][]string{db_type: *msg.Msg1}
+	return err
+}
 
+func (c *Cmds) Delete(msg *AMQP_Message) (err error) {
+	defer func() {
+		if pan := recover(); pan != nil {
+			msg1 := fmt.Sprintf("%s", pan)
+			erro = errors.New(msg1)
+		}
+	}()
+	db_type := msg.Object
+	switch db_type {
+	case "Domain":
+	case "Cmdb":
+		for k1, v1 := range *msg.Msg1 {
+			for k2, _ := range v1 {
+				(*msg).Msg1[k1][k2] = nil
+			}
+		}
+	case "Routedb":
+		for k, _ := range *msg.Msg1 {
+			(*msg).Msg1[k] = nil
+		}
+	}
+	*RT.Chan <- map[string]map[string]map[string]map[string][]string{db_type: *msg.Msg1}
 	return err
 }
