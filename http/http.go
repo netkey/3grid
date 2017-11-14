c90e2fbcb22f59cbba78c603812b9fb0fa7fb526
Zihua Ye <zihua.ye@qq.com>
2017-11-14 22:30:28 +0800

    201711142230

--- http/http.go
+++ http/http.go
@@ -8,7 +8,6 @@ import (
 	"compress/gzip"
 	"encoding/json"
 	"github.com/kavu/go_reuseport"
-	"io"
 	"net"
 	"net/http"
 	"strconv"
@@ -23,7 +22,7 @@ type HTTP_worker struct {
 	Rtdb     *RT.Route_db
 	Server   *http.Server
 	Handlers map[string]interface{}
-	Cache    map[string]map[string][]byte
+	Cache    map[string]map[string]map[string][]byte
 }
 
 func (hw *HTTP_worker) InitHandlers() *http.ServeMux {
@@ -40,28 +39,31 @@ func (hw *HTTP_worker) InitHandlers() *http.ServeMux {
 	return mux
 }
 
-func (hw *HTTP_worker) GetCache(dn, ipac string) *[]byte {
+func (hw *HTTP_worker) GetCache(dn, ipac, key string) *[]byte {
 	if hw.Cache[dn] != nil {
 		if hw.Cache[dn][ipac] != nil {
-			b := hw.Cache[dn][ipac]
-			if b == nil {
-				return nil
-			} else {
-				return &b
+			if hw.Cache[dn][ipac][key] != nil {
+				b := hw.Cache[dn][ipac][key]
+				if b != nil {
+					return &b
+				}
 			}
 		}
 	}
 	return nil
 }
 
-func (hw *HTTP_worker) UpdateCache(dn, ipac string, b *[]byte) {
+func (hw *HTTP_worker) UpdateCache(dn, ipac, key string, b *[]byte) {
 	if hw.Cache[dn] == nil {
-		hw.Cache[dn] = make(map[string][]byte)
+		hw.Cache[dn] = make(map[string]map[string][]byte)
 	}
 	if hw.Cache[dn][ipac] == nil {
-		hw.Cache[dn][ipac] = []byte{}
+		hw.Cache[dn][ipac] = make(map[string][]byte)
 	}
-	hw.Cache[dn][ipac] = append(hw.Cache[dn][ipac], *b...)
+	if hw.Cache[dn][ipac][key] == nil {
+		hw.Cache[dn][ipac][key] = []byte{}
+	}
+	hw.Cache[dn][ipac][key] = append(hw.Cache[dn][ipac][key], *b...)
 }
 
 //HTTP 302
@@ -76,9 +78,11 @@ func (hw *HTTP_worker) Http302(w http.ResponseWriter, r *http.Request) {
 func (hw *HTTP_worker) HttpDns(w http.ResponseWriter, r *http.Request) {
 	var err error
 	var body []byte
-	var dn, ips, ac, ipac string
+	var dn, ips, client_ips, ac, ipac, key string
 	var ip net.IP
 	var debug int = 0
+	var buf bytes.Buffer
+	var b []byte
 
 	defer func() {
 		if pan := recover(); pan != nil {
@@ -90,13 +94,15 @@ func (hw *HTTP_worker) HttpDns(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("X-GSLB", hw.Name)
 
 	if dna := r.URL.Query()["domain"]; dna == nil {
-		io.WriteString(w, "param error")
+		w.Write([]byte("param error"))
 		return
 	} else {
 		dn = dna[0]
 	}
+
+	client_ips = strings.Split(r.RemoteAddr, ":")[0]
 	if ipa := r.URL.Query()["ip"]; ipa == nil {
-		ips = strings.Split(r.RemoteAddr, ":")[0]
+		ips = client_ips
 	} else {
 		ips = ipa[0]
 	}
@@ -109,53 +115,58 @@ func (hw *HTTP_worker) HttpDns(w http.ResponseWriter, r *http.Request) {
 		ac = hw.Ipdb.GetAreaCode(ip)
 	}
 
-	aaa, ttl, _type, _, _, _, _ := hw.Rtdb.GetAAA(dn, ac, ip, debug)
-	if _type == "" {
-		_type = "A"
-	}
-
-	_dns_info := []string{_type, strconv.Itoa(int(ttl))}
-	_dns_info = append(_dns_info, aaa...)
-
 	if debug != 2 {
 		ipac = ips
 	} else {
 		ipac = ac
 	}
 
-	_pb := hw.GetCache(dn, ipac)
+	if r.Header["Accept-Encoding"] != nil {
+		w.Header().Set("Content-Encoding", "gzip")
+		key = "gzip"
+	} else {
+		key = "nc"
+	}
+
+	_pb := hw.GetCache(dn, ipac, key)
 	if _pb != nil {
 		body = *_pb
 	}
 
 	if body == nil {
-		w.Header().Set("X-Cache", "Miss")
 
-		if body, err = json.Marshal(map[string]map[string]map[string][]string{"dns": {dn: {ipac: _dns_info}}}); err == nil {
-			if false && r.Header["Accept-Encoding"] != nil {
-				var buf bytes.Buffer
-				var b []byte
+		aaa, ttl, _type, _, _, _, _ := hw.Rtdb.GetAAA(dn, ac, ip, debug)
+		if _type == "" {
+			_type = "A"
+		}
+
+		_dns_info := []string{_type, strconv.Itoa(int(ttl))}
+		_dns_info = append(_dns_info, aaa...)
+
+		body, err = json.Marshal(map[string]map[string]map[string][]string{"dns": {dn: {ipac: _dns_info}}})
+		if err == nil {
+			w.Header().Set("X-GSLB-Cache", "Miss")
+			if key == "gzip" {
 				g := gzip.NewWriter(&buf)
 				g.Write(body)
 				g.Close()
 
 				b = buf.Bytes()
-				w.Header().Set("Content-Encoding", "gzip")
 				w.Write(b)
-				hw.UpdateCache(dn, ipac, &b)
+				hw.UpdateCache(dn, ipac, key, &b)
 			} else {
 				w.Write(body)
-				hw.UpdateCache(dn, ipac, &body)
+				hw.UpdateCache(dn, ipac, key, &body)
 			}
 		} else {
 			w.Write([]byte("data error"))
 		}
 	} else {
-		w.Header().Set("X-Cache", "Hit")
+		w.Header().Set("X-GSLB-Cache", "Hit")
 		w.Write(body)
 	}
 
-	G.Outlog3(G.LOG_HTTP, "HttpDns %s %s", dn, ipac)
+	G.Outlog3(G.LOG_HTTP, "HttpDns %s %s %s", client_ips, dn, ipac)
 }
 
 func Working(myname, listen string, port string, num int, ipdb *IP.IP_db, rtdb *RT.Route_db) {
@@ -170,7 +181,7 @@ func Working(myname, listen string, port string, num int, ipdb *IP.IP_db, rtdb *
 	worker.Name = myname
 	worker.Ipdb = ipdb
 	worker.Rtdb = rtdb
-	worker.Cache = make(map[string]map[string][]byte)
+	worker.Cache = make(map[string]map[string]map[string][]byte)
 
 	if listener, err := reuseport.Listen("tcp", listen+":"+port); err != nil {
 		G.OutDebug2(G.LOG_GSLB, "Failed to listen port: %s", err)
